98531352c912f352b83eac069733bd81fa863f8b
Luke McCoy <44255214+lukemccoy27@users.noreply.github.com>
2026-02-25 15:49:14 +0000

    Create index.tsx

---
+++ src/slides/index.tsx
@@ -0,0 +1,31 @@
+import Slide01_Cover from "./Slide01_Cover";
+import Slide02_Problem from "./Slide02_Problem";
+import Slide03_Opportunity1 from "./Slide03_Opportunity1";
+import Slide04_Opportunity2 from "./Slide04_Opportunity2";
+import Slide05_Solution from "./Slide05_Solution";
+import Slide06_ProductSuite from "./Slide06_ProductSuite";
+import Slide07_Traction from "./Slide07_Traction";
+import Slide08_SportsPyramid from "./Slide08_SportsPyramid";
+import Slide09_FootballAustraliaGFX from "./Slide09_FootballAustraliaGFX";
+import Slide10_FuseAndControlRoom from "./Slide10_FuseAndControlRoom";
+import Slide11_E2EAutomation from "./Slide11_E2EAutomation";
+import Slide12_WhyLIGRWins from "./Slide12_WhyLIGRWins";
+import Slide13_EndFrame from "./Slide13_EndFrame";
+import Slide14_Farewell from "./Slide14_Farewell";
+
+export const slides = [
+  <Slide01_Cover />,
+  <Slide02_Problem />,
+  <Slide03_Opportunity1 />,
+  <Slide04_Opportunity2 />,
+  <Slide05_Solution />,
+  <Slide06_ProductSuite />,
+  <Slide07_Traction />,
+  <Slide08_SportsPyramid />,
+  <Slide09_FootballAustraliaGFX />,
+  <Slide10_FuseAndControlRoom />,
+  <Slide11_E2EAutomation />,
+  <Slide12_WhyLIGRWins />,
+  <Slide13_EndFrame />,
+  <Slide14_Farewell />
+];
